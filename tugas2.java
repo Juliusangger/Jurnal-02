f3bb4fa75521912578d18e45ed3b21255d81df50
JuliusASW <juliusangger1@gmail.com>
2021-10-01 10:39:11 +0700

    Praktikum

---
+++ tugas2.java
@@ -0,0 +1,11 @@
+public class tugas2 {
+    public static void main(String[] Args) {
+        double Ipk = 3.50;
+        String Nama = "Julius Angger Satrio Wicaksono";
+        long Nim = 6706213148L ;
+
+        System.out.println("Nama :" + Nama);
+        System.out.println("NIM : " + Nim);
+        System.out.println("IPK: " + Ipk);
+    }
+}
